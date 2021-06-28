8afb11698344537d8b885d8b7a7797173d70500d
andy910829 <71509005+andy910829@users.noreply.github.com>
2021-06-28 14:39:36 +0800

    Add files via upload

--- slm.py
+++ slm.py
@@ -28,19 +28,18 @@ def gethtml(url, name):
     return r.text
 
       
-# def writedown(html, file):
-#     soap = BeautifulSoup(html, "html.parser")
-#     str = ("本日漲停","開盤競價基準","本日跌停")
-#     file.write(str)
-#     for num in soap.select('tr').contents:
-#         #u_list.append(num[1].string, num[2].string, num[3].string)
-#         file.write(num[1].string, num[2].string, num[3].string)
-    
+def writedown(html, file, ulist):
+    soap = BeautifulSoup(html, "html.parser")
+    content = soap.tbody.tr('td')
+    title = ["本日漲停:", "開盤競價基準:", "本日跌停:"]
+    for num in content:       
+        ulist.append(str(num).strip('<td>').strip('</td>'))      
+
+    file.write(title[0]  + ulist[0] + title[1] + ulist[1] + title[2] + ulist[2] + '\n')
 
 def main():
     i = 0
     name = []
-    #u_list = []
 
     while (True): 
         name.append(input("請輸入代號，按0結束:"))
@@ -54,16 +53,17 @@ def main():
     driver = webdriver.Chrome("C:\\Users\\88696\\Dropbox\\我的電腦 (LAPTOP-SVUIPIGO)\\Desktop\\chromedriver.exe")
     driver.get("https://www.twse.com.tw/zh/stockSearch/stockSearch")
 
-    f = open("stock.txt","w",encoding = "utf-8")
+    f = open("stock.txt", "w", encoding = "utf-8")
  
-
+    ulist = []
     for count in range(i):
         search(name[count], driver)
         url = "https://www.twse.com.tw/zh/stockSearch/showStock?"
         gethtml(url, name[count])
-        #writedown(gethtml(url), f)
-        f.write(gethtml(url, name[count]))
-    
+        writedown(gethtml(url, name[count]), f, ulist)
+        
+
     f.close()
+    print("下載成功")
 main()    
 
