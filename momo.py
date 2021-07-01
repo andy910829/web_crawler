328c8d90b87b73e4f9a940e28e3e9b6388564dac
andy910829 <71509005+andy910829@users.noreply.github.com>
2021-07-02 02:52:03 +0800

    Add files via upload

--- momo.py
+++ momo.py
@@ -1,37 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+import os
 
 
 def search(keyword):
-    url = 'https://www.momomall.com.tw/mmlsearch/%s.html'
-    headers = {
-        'user-agent' : 'Mozilla/5.0'     
-    }
-    r = requests.post(url%keyword, headers = headers, timeout = 10)
-    r.raise_for_status()
-    r.encoding = 'UTF-8'
-    return r.text
+    try:
+        url = 'https://www.momomall.com.tw/mmlsearch/%s.html'
+        headers = {
+            'user-agent' : 'Mozilla/5.0'     
+        }
+        r = requests.post(url%keyword, headers = headers, timeout = 10)
+        r.raise_for_status()
+        r.encoding = 'UTF-8'
+        return r.text
+    except:
+        print('檢查網路問題!')
  
 
 def gethtml(html, list, list2):
-    soap = BeautifulSoup(html, 'html.parser')
-    price = soap.find_all('p', class_ = 'prdPrice')
-    name = soap.find_all(class_ = 'prdName')
-    for x in name:    
-        list.append(x)
-    for i in price:
-        list2.append(i)
-
+    try:
+        soap = BeautifulSoup(html, 'html.parser')
+        price = soap.find_all('p', class_ = 'prdPrice')
+        name  = soap.find_all(class_ = 'prdName')
+        for x in name:    
+            list.append(x)
+        for i in price:
+            list2.append(str(i))
+    except:
+        print('出現錯誤')
 
 def writeinfo(list, list2, f):
+    priz = []
     for num in range(len(list)):
         f.write('商品名稱:')
         f.write(str(list[num].text))
         f.write('\n')
         f.write('商品價格:')
-        f.write(str(list2[num].text))
+        priz.append('')
+        a = list2[num].find('$')
+        b = list2[num].find('</b>')
+        for i in range(a, b):
+            priz[num] += list2[num][i]
+        f.write(priz[num])
         f.write('\n')
-               
+                
 
 def main():
     N_list = []
@@ -42,5 +55,7 @@ def main():
     gethtml(search(kw), N_list, P_list)
     writeinfo(N_list, P_list, f)
     f.close()
+    print('下載完成')
+    os.system('pause')
     
 main()
